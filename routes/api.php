dd4be14dc220a21977b6f8064b58975a931668f4
Paul Thomas <paulthomas2107@gmail.com>
2022-10-26 21:02:13 +0100

    More...

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+    
+Route::get('/products', function() {
+    return 'products';
+});
+
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
